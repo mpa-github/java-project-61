fb0313852df01dc7f559472344ba677557577bfe
Pavel Muravyev <muravyev.pavel@gmail.com>
2023-01-22 21:59:09 +0300

    'Prime' and 'Even' game classes refactoring.

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -8,12 +8,14 @@ public class EvenGame {
     public static final String RULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
     private static final int MIN_POSSIBLE_VALUE = 0;
     private static final int MAX_POSSIBLE_VALUE = 100;
+    private static final String YES = "yes";
+    private static final String NO = "no";
 
     public static void start() {
         String[][] gameData = new String[Engine.GAME_ROUNDS][Engine.GAME_PARAMETERS];
         for (int i = 0; i < gameData.length; i++) {
             int numberToGuess = RandomIntegerUtils.generateNumber(MIN_POSSIBLE_VALUE, MAX_POSSIBLE_VALUE);
-            String correctAnswer = isEven(numberToGuess) ? "yes" : "no";
+            String correctAnswer = isEven(numberToGuess) ? YES : NO;
             gameData[i][0] = String.valueOf(numberToGuess);
             gameData[i][1] = correctAnswer;
         }
--- app/src/main/java/hexlet/code/games/PrimeGame.java
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -8,12 +8,14 @@ public class PrimeGame {
     public static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     private static final int MIN_POSSIBLE_VALUE = 2;
     private static final int MAX_POSSIBLE_VALUE = 90;
+    private static final String YES = "yes";
+    private static final String NO = "no";
 
     public static void start() {
         String[][] gameData = new String[Engine.GAME_ROUNDS][Engine.GAME_PARAMETERS];
         for (int i = 0; i < gameData.length; i++) {
             int number = RandomIntegerUtils.generateNumber(MIN_POSSIBLE_VALUE, MAX_POSSIBLE_VALUE);
-            String correctAnswer = isPrime(number) ? "yes" : "no";
+            String correctAnswer = isPrime(number) ? YES : NO;
             gameData[i][0] = String.valueOf(number);
             gameData[i][1] = correctAnswer;
         }
